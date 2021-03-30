9c3779e19ae861ddecde61eadfb3b523da980b6e
Edwin <edgoca1962@gmail.com>
2021-03-30 13:33:09 -0600

    Opcion Salir

--- cms_adm/vistas/contenidos/Navegacion.php
+++ cms_adm/vistas/contenidos/Navegacion.php
@@ -223,7 +223,7 @@
                   <div class="col-sm-6">
                      <ol class="breadcrumb float-sm-right">
                         <li class="breadcrumb-item"><a href="Inicio">Home</a></li>
-                        <li class="breadcrumb-item active">Starter Page</li>
+                        <li class="breadcrumb-item active"><a href="Ingreso">Salir</a></li>
                      </ol>
                   </div><!-- /.col -->
                </div><!-- /.row -->
