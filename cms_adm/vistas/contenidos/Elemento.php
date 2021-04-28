115e814e678cbc22a9a98e149dfe9d8d57cb1b5b
Edwin <edgoca1962@gmail.com>
2021-04-28 17:02:23 -0600

    Cambio formulario ingreso

--- cms_adm/vistas/contenidos/Elemento.php
+++ cms_adm/vistas/contenidos/Elemento.php
@@ -1,8 +1,1 @@
-<div class="container">
-   <div class="row editarImagen">
-      <canvas id="canvas" class="rounded-circle"></canvas>
-   </div>
-   <div class="row mt-4" style="width:600px; height:400px">
-      <img src="./vistas/dist/img/prueba_original.jpg" alt="Ejemplo de manejo de canvas con imagen">
-   </div>
-</div>
+<h1>Mantenimiento de Elementos.</h1>
